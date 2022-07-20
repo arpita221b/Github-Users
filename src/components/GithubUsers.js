26695176bc7f0a139389aee0adfc900a525f5c1c
arpita221b <navalarpita@gmail.com>
2022-07-20 19:47:58 +0530

    Add loading

--- src/components/GithubUsers.js
+++ src/components/GithubUsers.js
@@ -1,36 +1,41 @@
 import Box from "./Box";
 import { useEffect, useState } from "react";
+import Loading from "./Loading";
 
 const GithubUsers = () => {
   const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getUsers();
+
     async function getUsers() {
       try {
         const response = await fetch("https://api.github.com/users");
-
         const data = await response.json();
         setUserData(data);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
     }
   }, []);
 
-  return (
-    <div className="container">
-      {userData.map((user) => {
-        return (
-          <Box
-            src={user.avatar_url}
-            username={user.login}
-            repository={user.html_url}
-          />
-        );
-      })}
-    </div>
-  );
+  if (loading === true) return <Loading />;
+  else
+    return (
+      <div className="container">
+        {userData.map((user) => {
+          return (
+            <Box
+              src={user.avatar_url}
+              username={user.login}
+              repository={user.html_url}
+            />
+          );
+        })}
+      </div>
+    );
 };
 
 export default GithubUsers;
